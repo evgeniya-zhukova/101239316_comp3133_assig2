67b8cedbff16fbd01b4f60dcfc1be30455893d24
Evgeniya Zhukova <evgeniya.zhukova@georgebrown.ca>
2021-04-21 23:15:16 -0400

    Add some components

--- Angular-frontend/src/app/app.module.ts
+++ Angular-frontend/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HotelsComponent } from './hotels/hotels.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { LoginComponent } from './login/login.component';
+import { BookingsComponent } from './bookings/bookings.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HotelsComponent,
+    SignUpComponent,
+    LoginComponent,
+    BookingsComponent
   ],
   imports: [
     BrowserModule,
