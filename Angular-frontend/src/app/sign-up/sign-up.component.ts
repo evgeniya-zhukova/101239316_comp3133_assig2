d3a43cd75de44ea8d4f933247e009998832f8250
Evgeniya Zhukova <evgeniya.zhukova@georgebrown.ca>
2021-04-22 19:07:58 -0400

    Add functionality to Sign up page

--- Angular-frontend/src/app/sign-up/sign-up.component.ts
+++ Angular-frontend/src/app/sign-up/sign-up.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Apollo, gql } from 'apollo-angular';
+import { User } from '../models/user';
 
 @Component({
   selector: 'app-sign-up',
@@ -7,8 +9,67 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignUpComponent implements OnInit {
 
-  constructor() { }
+  users: User[];
+  loading = true;
+  error: any;
+  userForms = {
+    user_id: 0,
+    username: '',
+    password: '',
+    email: ''
+  };
+
+  constructor(private apollo: Apollo) { }
 
   ngOnInit(): void {
+    this.apollo
+      .query<any>({
+        query: gql`
+          {
+            getUser {
+                id,
+                user_id,
+                username,
+                password,
+                email
+            }
+          }
+        `
+      })
+      .subscribe(
+        ({ data, loading }) => {
+          this.users = data.getUser;
+          this.loading = loading;
+        },
+        error => {
+          this.loading = false;
+          this.error = error;
+        }
+      );
+  }
+
+  newUser(): void {
+    this.apollo.mutate({
+      mutation: gql`
+        mutation ($user_id:Int!, $username:String!, $password:String!, $email:String!){
+          addUser(user_id:$user_id, username:$username, password:$password, email:$email){
+            user_id,
+            username,
+            password,
+            email
+          }
+        }`,
+      variables: {
+          user_id: (this.users[this.users.length - 1].user_id + 1),
+          username: this.userForms.username,
+          password: this.userForms.password,
+          email: this.userForms.email
+      }
+    })
+      .subscribe(({data}) => {
+      const newUsers = Object.assign([], this.users);
+      newUsers.unshift(data['addUser']);
+      this.users = newUsers;
+    });
   }
 }
